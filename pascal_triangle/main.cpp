50df19501b1337e3f46620dcaf855fa33705f816
Marcelo Saguas Iacovone <marceloiaco@gmail.com>
2023-11-19 12:21:52 -0800

    cleanup and new solution for reduciton

--- pascal_triangle/main.cpp
+++ pascal_triangle/main.cpp
@@ -2,55 +2,55 @@
 #include <vector>
 
 class Solution {
-public:
-  std::vector<std::vector<int>> generate(int numRows) {
-    std::vector<std::vector<int>> result;
-    // Return empty vector of a vector if no numRows
-    if (numRows <= 0) {
-      return result;
-    }
-
-    for (int i{0}; i < numRows; i++) {
-      std::vector<int> internal_vec;
-
-      // Add left side 1
-      internal_vec.push_back(1);
-
-      // Add middle
-      if (i > 1) {
-        for (int j{0}; j < i - 1; j++) {
-          int top_left = result.at(i - 1).at(j);
-          int top_right = result.at(i - 1).at(j + 1);
-          internal_vec.push_back(top_left + top_right);
+  public:
+    std::vector<std::vector<int>> generate(int numRows) {
+        std::vector<std::vector<int>> result;
+        // Return empty vector of a vector if no numRows
+        if (numRows <= 0) {
+            return result;
         }
-      }
 
-      // Add right side 1
-      if (i != 0) {
-        internal_vec.push_back(1);
-      }
-      result.push_back(internal_vec);
+        for (int i{0}; i < numRows; i++) {
+            std::vector<int> internal_vec;
+
+            // Add left side 1
+            internal_vec.push_back(1);
+
+            // Add middle
+            if (i > 1) {
+                for (int j{0}; j < i - 1; j++) {
+                    int top_left = result.at(i - 1).at(j);
+                    int top_right = result.at(i - 1).at(j + 1);
+                    internal_vec.push_back(top_left + top_right);
+                }
+            }
+
+            // Add right side 1
+            if (i != 0) {
+                internal_vec.push_back(1);
+            }
+            result.push_back(internal_vec);
+        }
+        return result;
     }
-    return result;
-  }
 };
 
 int main() {
-  Solution sl{};
-  std::vector<std::vector<int>> result = sl.generate(5);
-  std::cout << "[";
-  for (size_t i{0}; i < result.size(); ++i) {
+    Solution sl{};
+    std::vector<std::vector<int>> result = sl.generate(5);
     std::cout << "[";
-    for (size_t j{0}; j < result.at(i).size(); ++j) {
-      std::cout << result.at(i).at(j);
-      if (j != result.at(i).size() - 1) {
-        std::cout << ",";
-      }
+    for (size_t i{0}; i < result.size(); ++i) {
+        std::cout << "[";
+        for (size_t j{0}; j < result.at(i).size(); ++j) {
+            std::cout << result.at(i).at(j);
+            if (j != result.at(i).size() - 1) {
+                std::cout << ",";
+            }
+        }
+        std::cout << "]";
+        if (i != result.size() - 1) {
+            std::cout << ",";
+        }
     }
-    std::cout << "]";
-    if (i != result.size() - 1) {
-      std::cout << ",";
-    }
-  }
-  std::cout << "]" << std::endl;
+    std::cout << "]" << std::endl;
 }
--- reduction/main.cpp
+++ reduction/main.cpp
@@ -1,72 +1,47 @@
 #include <algorithm>
 #include <iostream>
-#include <limits.h>
 #include <vector>
 
 class Solution {
-public:
-  int reductionOperations(std::vector<int>& nums) {
-    // If it's of length 1, return 0 operations
-    if (nums.size() == 1) {
-      return 0;
+  public:
+    int reductionOperations(std::vector<int>& nums) {
+        // If it's of length 1, return 0 operations
+        if (nums.size() == 1) {
+            return 0;
+        }
+
+        int result{0};
+        int operations{0};
+        std::sort(nums.begin(), nums.end());
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (nums.at(i) != nums.at(i - 1)) {
+                ++operations;
+            }
+            result += operations;
+        }
+
+        return result;
     }
-
-    int largest{INT_MIN};
-    int largest_i{0};
-    int nextLargest{INT_MIN};
-    int operations{0};
-    bool allEqual{true};
-    std::sort(nums.begin(), nums.end());
-    do {
-      // Check if all equal
-      allEqual = true;
-      for (size_t i = 0; i < nums.size() - 1; i++) {
-        if (nums.at(i) != nums.at(i + 1)) {
-          allEqual = false;
-          break;
-        }
-      }
-      if (allEqual) {
-        break;
-      }
-
-      // Find largest
-      for (size_t i = nums.size() - 1; i > 0; i--) {
-        if (nums.at(i) > nums.at(i - 1)) {
-          largest = nums.at(i);
-          largest_i = i;
-          break;
-        }
-      }
-      nextLargest = nums.at(largest_i - 1);
-
-      // Reduce
-      nums.at(largest_i) = nextLargest;
-      operations++;
-    } while (!allEqual);
-
-    return operations;
-  }
 };
 
 
 int main() {
-  Solution sl{};
-  std::vector<std::vector<int>> numstests = {
-      {5, 1, 3},
-      {1, 1, 1},
-      {1, 1, 2, 3, 3}};
-  for (auto&& nums : numstests) {
-    int result = sl.reductionOperations(nums);
+    Solution sl{};
+    std::vector<std::vector<int>> numstests = {
+        {5, 1, 3},
+        {1, 1, 1},
+        {1, 1, 2, 2, 3}};
+    for (auto&& nums : numstests) {
+        int result = sl.reductionOperations(nums);
 
-    // print tests
-    std::cout << "{";
-    for (size_t i = 0; i < nums.size(); i++) {
-      std::cout << nums.at(i);
-      if (i < nums.size() - 1) {
-        std::cout << ", ";
-      }
+        // print tests
+        std::cout << "{";
+        for (size_t i = 0; i < nums.size(); i++) {
+            std::cout << nums.at(i);
+            if (i < nums.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "} = " << result << std::endl;
     }
-    std::cout << "} = " << result << std::endl;
-  }
 }
